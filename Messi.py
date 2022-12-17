5fd799a5fd3bb7b20a7af4d3e228a0f268ed0a4a
daking72 <75477438+daking72@users.noreply.github.com>
2022-12-17 13:10:00 +0100

    Create Messi.py

---
+++ Messi.py
@@ -0,0 +1,89 @@
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^C^@�^@^A^@^@^>
+^@@^@ ^@^^^@^F^@^@^@^D^@^@^@@^@^@^@^@^@^@^@>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^Q^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^P^@^@^@^@^@^@^@c^M^E^@^@^@^B^@y
+^@^@^@^@^@^@^@^@^@^@^A^@^@^@�^@^@^@^A^@^@^@>
+�^@^@^@�qX^\���|BE���nE��^Z�Z�^@^@^@�^@^@^@>
+^@^@^@T^@^@^@~^@^@^@�^@^@^@�^@^@^@1^@^@^@�^>
+^?^@^@^@^@^@^@�,^C^@^@^@^@^@^C^D^@^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@�M^C^@^@^@^@^@^B^D^@^>
+██   ██ ████   ██ ██   ██ ████  ████
+
+ ^[[1;33m██████  ██ ██  ██ ██   ██ ██ ████ >
+
+ ██   ██ ██  ██ ██ ██   ██ ██  ██  ██
+
+ ^[[1;36m██   ██ ██   ████ ██████  ██      >
+
+(!)═══════════════════════════════════════
+
+(!) Author   : David
+
+(!) Guthub   : Daking72 
+
+(!) Facebook : David Abdulrazaq 
+
+(!) Type     : Trail
+
+(!) Enjoy Trail Share SS in Group ❤
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m[2] Random Cloning ^[[1;32m[M2]
+
+^[[1;37m[0] Exit Menu ^@2^@^[[38;5;208m^@6^>
+
+[•] Total OK Ids : ^@ConnectionError^@D^@Di>
+
+[•] Limint : ^@F^@G^@H^@I^@
+
+(!) Info : Something Working Was Wrong^@J^@>
+
+[•] CP IDS SAVE : /sdcard/DKING-CP.txt^@P^@>
+
+[•] 9JA Code: 080625,070239,090250,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+•] USA Code: +1456680,+1455678,+1488907,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+
+^@^M^@; ^@.^@(^@!^@) ^@  ^@^M ^@[^@a^@aa^@a>
+
+return next yielded value or raise StopIter>
+
+return next yielded value or raise StopIter>
+
+    try: Generator = _module.Generator
+
+    except AttributeError: pass
+
+    else: Generator.register(_cython_genera>
+
+if _cython_coroutine_type is not None:
+
+    try: Coroutine = _module.Coroutine
+
+    except AttributeError: pass
+
+^@need more than %zd value%.1s to unpack^@_>
+^@^@��^@^@
+^@^@x�^@^@@
+^@^@^P�^@^@`
+^@^@��^@^@�
+^@^@8�^@^@�
+^@^@x�^@^@�
+^@^@^T�^@^@�
+^@^@��^@^@^@^K^@^@��^@^@ ^K^@^@��^@^@@^K^@^>
+�^L�^N�^P�^R�^T�^V�^X^@^@^@^\^@^@^@^\^A^@^@>
+�^L^@^\^@^@^@\^A^@^@��^@^@�^@^@^@^@L^L^] �^>
+    else: Coroutine.register(_cython_corout>
